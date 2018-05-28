267d76d10a233f8e432331d5c1788810e61b7dd3
Rob Indesteege <rob.indesteege@icapps.com>
2018-05-28 10:25:35 +0200

    add toggle component

---
+++ src/Toggle.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Toggle extends Component {
+  state = {
+    toggleOn: false
+  };
+
+  toggleDiv = () => {
+    this.setState(state => ({ toggleOn: !state.toggleOn }));
+  };
+  render() {
+    const { toggleOn } = this.state;
+    return (
+      <div>
+        <button onClick={this.toggleDiv}>Toggle my div</button>
+        <div style={{ backgroundColor: toggleOn ? "green" : "red" }}>
+          {toggleOn ? "On" : "Off"}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Toggle;
