d63accb51281f0cdb3c492f89e0eeb914942c9f5
Rob Indesteege <rob.indesteege@icapps.com>
2018-05-28 13:16:29 +0200

    add comments nuances

--- src/Toggle.test.js
+++ src/Toggle.test.js
@@ -5,18 +5,18 @@ import Toggle from './Toggle';
 describe('Toggle', () => {
   it('initial state for the toggle should be false', () => {
     const wrapper = shallow(<Toggle />);
-    expect(wrapper.instance().state.toggleOn).toBe(false);
+    expect(wrapper.instance().state.toggleOn).toBe(false); // instance allows us to access the component as we would normally
   });
 
   it('changes the toggle state when toggleDiv is called', () => {
     const wrapper = shallow(<Toggle />);
     wrapper.instance().toggleDiv();
-    expect(wrapper.instance().state.toggleOn).toBe(true);
+    expect(wrapper.instance().state.toggleOn).toBe(true); //
   });
 
   it('changes the toggle state when the button is clicked', () => {
     const wrapper = mount(<Toggle />);
     wrapper.find('#myButton').simulate('click');
-    expect(wrapper.instance().state.toggleOn).toBe(true);
+    expect(wrapper.state().toggleOn).toBe(true); // without instance we can call the state() function
   });
 });
